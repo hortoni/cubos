ef6e04a6b426dc278fe3a9e5e79eb4b4850b1a45
hortoni <hortonny@hotmail.com>
2019-03-18 13:39:04 -0300

    add icon

--- app/src/main/java/xyz/manolos/cubos/movie/MoviePagerAdapter.kt
+++ app/src/main/java/xyz/manolos/cubos/movie/MoviePagerAdapter.kt
@@ -5,22 +5,27 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
+private const val ID_ACTION: Long = 28
+private const val ID_DRAMA: Long = 18
+private const val ID_FANTASY: Long = 14
+private const val ID_FICTION: Long = 878
+
 class MoviePagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
         val fragment = MovieFragment()
         val bundle = Bundle()
         when (position) {
             0 -> {
-                bundle.putLong("id", 28)
+                bundle.putLong("id", ID_ACTION)
             }
             1 -> {
-                bundle.putLong("id", 18)
+                bundle.putLong("id", ID_DRAMA)
             }
             2 -> {
-                bundle.putLong("id", 14)
+                bundle.putLong("id", ID_FANTASY)
             }
             else -> {
-                bundle.putLong("id", 878)
+                bundle.putLong("id", ID_FICTION)
             }
         }
         fragment.arguments = bundle
@@ -33,7 +38,7 @@ class MoviePagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence {
         return when (position) {
-            0 ->  "Ação"
+            0 -> "Ação"
             1 -> "Drama"
             2 -> "Fantasia"
             else -> {
